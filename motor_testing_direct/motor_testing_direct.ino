1dcc2e6d743ddf7e78e36c5cd3b578b7cf20b9f7
Raymond <raymond.yang121@outlook.com>
2021-05-30 01:12:46 -0400

    completed (untested) code for direct motor testing

--- motor_testing_direct/motor_testing_direct.ino
+++ motor_testing_direct/motor_testing_direct.ino
@@ -5,7 +5,7 @@
 #define MISO 12
 #define SCK 13
 #define CS0 10
-
+const byte address = 0x00;
 
 int ledPin = 5;
 int buttonApin = 2; //on uno, pin 2 and 3 allow interrupts
@@ -15,7 +15,6 @@ bool on = false;
 const byte motorControl = 9;
 const byte motorPower = 4;
 const byte wiper = A0;
-const byte main = 5;
 //ISR function declaration
 void ISR_LED();
 
@@ -30,51 +29,50 @@ void setup() {
   //SPI clock speed:10MHz, Data Shift:MSB First, Data Clock Idle: SPI_MODE0 (MODE =00)
   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0)); //comment if not MCP4161
   SPI.begin();
+  digitalPotWrite(CS0, address, 0);
 
 
   //set up interrupts
   attachInterrupt(digitalPinToInterrupt(buttonApin), ISR_LED, FALLING);
   //Note: FALLING is for when the pin goes from high to low.
 
-  serial.begin(9600); 
+  Serial.begin(9600); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //do nothing it the motor is not turned on
   if (!on) {
-    digitalWrite(ledPin, LOW);  
-    digitalWrite(main, HIGH);
     return; 
   }
   
   digitalWrite(ledPin, HIGH); 
-  digitalWrite(main, LOW);
   
   //read from POT
   int wiperValue = analogRead(wiper);  //note analogRead can return 0 to 1023
   
   
-  //map the wipeValue to the PWM control for the motor
-  int motorControlValue = map(wiperValue, 5, 1023, 0, 180); //note: servo motor rotates from 0 to 180
+  //map the wipeValue to the control for the motor
+  int motorControlValue = map(wiperValue, 5, 1023, 0, 255); 
   
-  //write to control servoMotor
-  servoObj.write(motorControlValue);
-  //delay(100);
-  Serial.println(motorControlValue);
+  digitalPotWrite(CS0, address, motorControlValue);
 
 }
 
 void ISR_LED() {
   //ledState = !ledState;
   on = !on;
-  
   Serial.print("Button Pressed\n");
+
+  if (!on) {
+    digitalWrite(ledPin, LOW);  
+    digitalPotWrite(CS0, address, 0);
+  }
 }
 
-void digitalPotWrite(int cs, int, address, int value) {
+void digitalPotWrite(int CS, int address, int command) {
   digitalWrite(CS, LOW);
   SPI.transfer(address); //if MCP4161
   //SPI.transfer(B00010001); //if MCP41010
-  SPI.transfer(value);
+  SPI.transfer(command);
   digitalWrite(CS, HIGH);
 }
